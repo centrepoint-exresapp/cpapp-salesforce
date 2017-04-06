e70c9b67722d5d44729a3a0ce937314b8222d0e6
lucymonie <lucy.monie.hall@gmail.com>
2017-04-06 17:41:13 +0100

    Cobbles together a local working version of app

--- src/routes/salesforce.js
+++ src/routes/salesforce.js
@@ -3,47 +3,49 @@ const authMiddleware = require('../helpers/authMiddleware.js');
 const nforce = require('nforce');
 const hapi = require('hapi');
 const dateHelper = require('../helpers/dateHelper.js');
+const userHelper = require('../helpers/userHelper');
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 const securityToken = process.env.SECURITY_TOKEN;
 
 
 module.exports = {
-  method: 'GET',
+  method: 'POST',
   path:'/salesforce',
-  // config: {
-  //   auth: {
-  //     strategy: 'base'
-  //   },
-  //   pre: [
-  //       { method: authMiddleware, assign: 'user' }
-  //   ],
+  config: {
+    auth: {
+      strategy: 'base'
+    },
+    pre: [
+        { method: authMiddleware, assign: 'user' }
+    ],
     handler: (req, reply) => {
-      // This var is going to need to come in on the request obj
-      let name = 'Lucy';
-      const org = nforce.createConnection({
-        clientId: process.env.SALESFORCE_KEY,
-        clientSecret: process.env.SALESFORCE_SECRET,
-        redirectUri: process.env.SF_REDIRECT,
-        environment: 'sandbox',
-        mode: 'single'
-      });
+      if (req.payload.rentalHistory === 'on') {
+        let firstName = userHelper.getFirstName(req.pre.user.givenNames);
+        const org = nforce.createConnection({
+          clientId: process.env.SALESFORCE_KEY,
+          clientSecret: process.env.SALESFORCE_SECRET,
+          redirectUri: process.env.SF_REDIRECT,
+          environment: 'sandbox',
+          mode: 'single'
+        });
 
-      org.authenticate({ username: username, password: password, securityToken: securityToken }, function(err, resp) {
-        let accomHistory;
-        if(!err) {
-          org.query({ query: `SELECT Start_Date__c, End_Date__c FROM Bedspace_Status__c WHERE Client__c IN (SELECT Id FROM Contact WHERE Contact.FirstName='Lucy')`}, function(err, res) {
-            if(err) return console.error(err);
-            else
-            accomHistory = res.records[0]._fields;
-            accomHistory = dateHelper.fixAccomDates(accomHistory);
-            accomHistory.name = name;
-            reply.view('data', accomHistory);
-          });
-        } else {
-          console.log('Error: ' + err.message);
-        }
-      });
+        org.authenticate({ username: username, password: password, securityToken: securityToken }, function(err, resp) {
+          let accomHistory;
+          if(!err) {
+            org.query({ query: `SELECT Start_Date__c, End_Date__c FROM Bedspace_Status__c WHERE Client__c IN (SELECT Id FROM Contact WHERE Contact.FirstName='${firstName}')`}, function(err, res) {
+              if(err) return console.error(err);
+              else
+              accomHistory = res.records[0]._fields;
+              accomHistory = dateHelper.fixAccomDates(accomHistory);
+              accomHistory.name = firstName;
+              reply.view('data', accomHistory);
+            });
+          } else {
+            console.log('Error: ' + err.message);
+          }
+        });
+      }
     }
-    // }
+  }
 };
