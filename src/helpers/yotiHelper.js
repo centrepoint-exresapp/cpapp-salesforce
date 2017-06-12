2d07f6a75e3e65a50859671d20e1ad7e78ee028f
lucymonie <lucy.monie.hall@gmail.com>
2017-06-12 23:31:33 +0100

    Updates yotihelper to use token only once per login

--- src/helpers/yotiHelper.js
+++ src/helpers/yotiHelper.js
@@ -3,11 +3,16 @@ const userTable = require('../database/tables/users');
 const userHelper = require('../helpers/userHelper');
 const errorMessages = require('../constants/errorMessages.js');
 
+let localStorage = {}
+
 module.exports = function (token, callback) {
-  yotiClient.getClient()
-  .getActivityDetails(token)
-  .then((activityDetails) => {
+  if (!localStorage.token) {
+    yotiClient.getClient()
+    .getActivityDetails(token)
+    .then((activityDetails) => {
     let user = userHelper.getUser(activityDetails);
+    localStorage.token = token;
+    localStorage.user = user;
     userTable.getUserId(user, function (err, userData) {
       if (err) {
         callback(errorMessages.databaseError);
@@ -18,7 +23,33 @@ module.exports = function (token, callback) {
       }
       callback(null, user);
     });
-  }).catch((err) => {
-    callback(errorMessages.loginError);
-  });
+    }).catch((err) => {
+      callback(errorMessages.loginError);
+    });
+  } else {
+    callback (null, localStorage.user);
+  }
 };
+
+
+
+// Original version
+// module.exports = function (token, callback) {
+//   yotiClient.getClient()
+//   .getActivityDetails(token)
+//   .then((activityDetails) => {
+//     let user = userHelper.getUser(activityDetails);
+//     userTable.getUserId(user, function (err, userData) {
+//       if (err) {
+//         callback(errorMessages.databaseError);
+//       }
+//       if (userData) {
+//         user.exists = true;
+//         user.admin = userData.admin;
+//       }
+//       callback(null, user);
+//     });
+//   }).catch((err) => {
+//     callback(errorMessages.loginError);
+//   });
+// };
